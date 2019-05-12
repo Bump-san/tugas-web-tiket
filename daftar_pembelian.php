ce5267ce98a08ec4c1c8c0817008977e539fd0c4
Ghassan <fsghassan2429d@gmail.com>
2019-05-12 21:29:44 +0700

    fix eui

--- daftar_pembelian.php
+++ daftar_pembelian.php
@@ -28,10 +28,10 @@
       $n = mysqli_num_rows($sql1);
 
       echo '<div class="container">';
-      echo "<table>"; // start a table tag in the HTML
+      echo "<table>";
 
-      echo "<tr><th> id </th><th> nama </th><th> email </th><th> tanggal_berangkat </th><th> kota asal </th><th> kota tujuan </th><th> jumlah penumpang </th><th> kelas </th><th> harga pertiket </th><th> total biaya </th></tr>";  //$row['index'] the index here is a field name
-      while($row = mysqli_fetch_array($sql1)){   //Creates a loop to loop through results
+      echo "<tr><th> id </th><th> nama </th><th> email </th><th> tanggal_berangkat </th><th> kota asal </th><th> kota tujuan </th><th> jumlah penumpang </th><th> kelas </th><th> harga pertiket </th><th> total biaya </th></tr>";
+      while($row = mysqli_fetch_array($sql1)){
         echo "<tr><td>" . $row['id'] . "</td><td>" . $row['nama'] . "</td><td>" . $row['email'] . "</td><td>" . $row['tanggal_berangkat'] . "</td><td>" . $row['kota_asal'] . "</td><td>" . $row['kota_tujuan'] . "</td><td>" . $row['jumlah_penumpang'] . "</td><td>" . $row['kelas'] . "</td><td> Rp. " . $row['harga_tiket'] . "</td><td> Rp. " . $row['total_biaya'] . "</td></tr>";
       }
 
--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
     <title>Tugas kuis Web Traveling</title>
-    <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./css/navbar.css">
     <link rel="stylesheet" href="./css/responsive.css">
     <link rel="stylesheet" href="./css/slideshow.css">
+    <link rel="stylesheet" href="./css/index.css">
 
     <!-- <style media="screen">
         .container {
@@ -64,6 +64,91 @@
 <!-- slideshow selesai -->
 
     <div class="container">
+
+      <div class="row tengah">
+
+      <div class="col-50">
+        <h3>Honest Price</h3>
+        <h4>No transaction fee. No hidden charges</h4>
+      </div>
+
+      <div class="col-50">
+        <img src="./gambar/traveloka/1.webp" style="width:200px">
+      </div>
+
+      </div>
+
+      <div class="row tengah">
+        <div class="col-50">
+          <img src="./gambar/traveloka/2.webp" style="width:200px">
+        </div>
+      <div class="col-50">
+        <h3>Various Payment Options</h3>
+        <h4>Be more flexible with various payment methods from ATM Transfer, Credit Card, to Internet Banking.</h4>
+      </div>
+      </div>
+
+      <div class="row tengah">
+      <div class="col-50">
+        <h3>Special Discounts</h3>
+        <h4>Special discounted hotels for flight customers, newsletter subscribers and Traveloka members.</h4>
+      </div>
+      <div class="col-50">
+        <img src="./gambar/traveloka/3.webp" style="width:200px">
+      </div>
+      </div>
+
+      <div class="row tengah">
+      <div class="col-50">
+        <img src="./gambar/traveloka/4.webp" style="width:200px">
+      </div>
+      <div class="col-50">
+        <h3>Best Price Guarantee</h3>
+          <h4>Competitive price from extensive network budget hotels to 5 star hotels such as Ibis, Amaris, Santika Hotel with best price guarantee.</h4>
+          </div>
+      </div>
+
+      <div class="row tengah">
+      <div class="col-50">
+        <h3>Real Guest Reviews</h3>
+        <h4>2,000,000+ verified reviews from real hotel guests will help you make the right decision.</h4>
+      </div>
+
+      <div class="col-50">
+        <img src="./gambar/traveloka/5.webp" style="width:200px">
+      </div>
+
+      </div>
+
+      <div class="row tengah">
+        <div class="col-50">
+          <img src="./gambar/traveloka/6.webp" style="width:200px">
+        </div>
+
+      <div class="col-50">
+        <h3>StayGuarantee</h3>
+        <h4>You're guaranteed a hotel stay, period. In the event of overbooked rooms or problems with your booking, we'll recommend a similar hotel for you at no additional costs. Or your money back.</h4>
+      </div>
+
+
+      </div>
+
+      <div class="row tengah">
+
+      <div class="col-50">
+        <img src="./gambar/demitugas.jpg" style="width:200px">
+      </div>
+
+      <div class="col-50">
+        <p>Sebenernya karena ada tugas aja sih</p>
+        <blockquote>
+          DEMI TU...
+          GAS
+          <footer> — JUKI</footer>
+        </blockquote>
+      </div>
+
+    </div>
 
     </div>
 
