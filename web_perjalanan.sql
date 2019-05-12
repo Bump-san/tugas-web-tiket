32c4950d0ede8718867e4990c76ee84518e2d793
Ghassan Fs <fsghassan2429d@gmail.com>
2019-05-12 13:47:12 +0700

    tambah export db

---
+++ web_perjalanan.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2019 at 08:46 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_perjalanan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history_pembelian`
+--
+
+CREATE TABLE `history_pembelian` (
+  `id` int(5) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `tanggal_berangkat` varchar(10) NOT NULL,
+  `kota_asal` varchar(20) NOT NULL,
+  `kota_tujuan` varchar(20) NOT NULL,
+  `jumlah_penumpang` varchar(5) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `total_biaya` varchar(20) NOT NULL,
+  `harga_tiket` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `history_pembelian`
+--
+
+INSERT INTO `history_pembelian` (`id`, `nama`, `email`, `tanggal_berangkat`, `kota_asal`, `kota_tujuan`, `jumlah_penumpang`, `kelas`, `total_biaya`, `harga_tiket`) VALUES
+(14, 'Ghassan', 'fsghassan2429d@gmail', '2019-05-21', 'Jakarta', 'Jakarta', '1', 'Ekonomi', '400,000', '400000'),
+(15, 'Ghassan', 'fsghassan2429d@gmail', '2019-05-11', 'Jakarta', 'Jakarta', '1', 'Ekonomi', '400,000', '400000'),
+(16, 'Ghassan', 'fsghassan2429d@gmail', '2019-05-11', 'Jakarta', 'Jakarta', '1', 'Ekonomi', '400,000', '400000'),
+(17, 'Ghassan', 'fsghassan2429d@gmail', '2019-05-22', 'Jakarta', 'Bali', '1', 'VIP', '1,700,000', '1700000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota`
+--
+
+CREATE TABLE `kota` (
+  `id` varchar(5) NOT NULL,
+  `nama_kota` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kota`
+--
+
+INSERT INTO `kota` (`id`, `nama_kota`) VALUES
+('1', 'Jakarta'),
+('2', 'Bandung'),
+('3', 'Semarang'),
+('4', 'Bali'),
+('5', 'Palembang');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `history_pembelian`
+--
+ALTER TABLE `history_pembelian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kota`
+--
+ALTER TABLE `kota`
+  ADD PRIMARY KEY (`id`(2));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `history_pembelian`
+--
+ALTER TABLE `history_pembelian`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
